02a54110319bba3dfcf21b1aee46cc51b38d8a15
Tanaka, Yuki <yuki_tanaka@r.recruit.co.jp>
2016-11-30 12:56:12 +0900

    completed all PoC for purifycss with grunt

---
+++ purifycss/README.md
@@ -0,0 +1,5 @@
+# PoC for purifycss
+Purify css cleans up your dirty css code containing unused selector and styles.
+
+## Dependencies
+grunt/grunt-purifycss or just install purifycss from npm if you don't want to use grunt as task runner.
---
+++ purifycss/gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    purifycss:{
+                target:{
+                         src: ['*.html', 'js/*.js'],
+                         css: ['css/bootstrap-full.css'],
+                         dest: 'css/bootstrap-grunt.css'
+                       }
+              }
+  });
+  grunt.loadNpmTasks('grunt-purifycss');
+}
